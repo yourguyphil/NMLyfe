89be29cb902f26afd9bf5ab56a63ab36b4577bb3
yourguyphil <philips.nguyen96@gmail.com>
2018-04-12 23:54:05 -0500

    Showing Peter How to Use Github

--- SpikeHelper/Program.cs
+++ SpikeHelper/Program.cs
@@ -14,6 +14,7 @@ namespace SpikeHelper
         [STAThread]
         static void Main()
         {
+            // Hello Peter
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Initial());
